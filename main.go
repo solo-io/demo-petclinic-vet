0adc6b44eb41f31a93c1dce1d630a352fb698939
Ashish Shrestha <axhixh@gmail.com>
2018-05-15 12:15:11 -0400

    support for new vet form

--- main.go
+++ main.go
@@ -13,17 +13,29 @@ func main() {
 		port = "8080"
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		vetList, err := vets()
-		if err != nil {
-			http.Error(w, fmt.Sprintf("unable to get vets: %q\n", err), 500)
-			return
-		}
-		if err := pageTemplate.Execute(w, vetList); err != nil {
-			http.Error(w, fmt.Sprintf("unable to execute template: %q\n", err), 500)
-		}
-	})
+	http.HandleFunc("/vets/new", newVetForm)
+	http.HandleFunc("/", getVets)
 
 	fmt.Printf("Starting HTTP server at port %s\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func getVets(w http.ResponseWriter, r *http.Request) {
+	vetList, err := vets()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("unable to get vets: %q\n", err),
+			http.StatusInternalServerError)
+		return
+	}
+	if err := pageTemplate.Execute(w, vetList); err != nil {
+		http.Error(w, fmt.Sprintf("unable to execute template: %q\n", err),
+			http.StatusInternalServerError)
+	}
+}
+
+func newVetForm(w http.ResponseWriter, r *http.Request) {
+	if err := formTemplate.Execute(w, nil); err != nil {
+		http.Error(w, fmt.Sprintf("unable to execute form template: %q", err),
+			http.StatusInternalServerError)
+	}
 }
